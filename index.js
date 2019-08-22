ca457fb1d5fd958a975daf2fb8d54473866df581
Donny Techiera <DonnyTech20@gmail.com>
2019-08-22 18:27:21 -0400

    http server added

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const fs = require('fs');
+const http = require('http');
 
 const json = fs.readFileSync(`${__dirname}/data.json`, 'utf-8');
+const laptopData = JSON.parse(json);
 
-console.log(__dirname);
-//console.log(json);
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.end('This is the response!');
+});
+
+
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Listening for req now');
+});
